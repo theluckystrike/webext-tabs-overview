a22b780516a68a334125b9d738ef4dc775773b16
theluckystrike <theluckystrike@users.noreply.github.com>
2026-03-04 05:20:48 +0700

    docs(jsdoc): add module-level JSDoc to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
+/**
+ * webext-tabs-overview - Tab overview component for web extensions
+ * @module webext-tabs-overview
+ * @description Display overview of all tabs with statistics
+ */
 export { TabsOverview, TabsOverviewError, TabsOverviewErrorCode, TabStats } from './overview';
